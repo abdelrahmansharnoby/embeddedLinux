11527aeda8fb024e421828dc7f3c84bfbacbf6ee
Abdelrahman Ahmed <sharnoby@pop-os.localdomain>
2023-08-07 12:20:27 +0300

    Task 1 done

---
+++ task1.py
@@ -0,0 +1,6 @@
+x = input("please enter User Name\n")
+y = int(input("please enter password\n"))
+if (x=="Ahmed"and y== 1394 ) or (x=="Ali" and y== 6078) or (x=="Amr" and y == 9345):
+    print("Welcome :)")
+else:
+    print("incoreect entry")
